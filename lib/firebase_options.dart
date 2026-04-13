ddbadb167bd59c4f2324865c039c2af5ab2e1828
Mimo <mimo@example.com>
2026-04-13 14:07:04 +0200

    Carte Nabil: mise a jour application

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,14 +1,41 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
-    return const FirebaseOptions(
-      apiKey: 'AIzaSyBUN9UbDnsD0K5E5DxGpGaoQs2xpLnNayE',
-      appId: '1:911900427556:web:230e73ea3b3d1c0db51fb1',
-      messagingSenderId: '911900427556',
-      projectId: 'elite-by-s',
-      authDomain: 'elite-by-s.firebaseapp.com',
-      storageBucket: 'elite-by-s.firebasestorage.app',
-    );
+    if (kIsWeb) {
+      return web;
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for ios - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
   }
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyBUN9UbDnsD0K5E5DxGpGaoQs2xpLnNayE',
+    appId: '1:911900427556:web:230e73ea3b3d1c0db51fb1',
+    messagingSenderId: '911900427556',
+    projectId: 'elite-by-s',
+    authDomain: 'elite-by-s.firebaseapp.com',
+    storageBucket: 'elite-by-s.firebasestorage.app',
+  );
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyALftq8qXtQALSE3U1Eq-weKUdd5olykA8',
+    appId: '1:911900427556:android:48b3d6dcd96cc0e1b51fb1',
+    messagingSenderId: '911900427556',
+    projectId: 'elite-by-s',
+    storageBucket: 'elite-by-s.firebasestorage.app',
+  );
 }
