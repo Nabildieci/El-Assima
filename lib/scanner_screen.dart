1eaf3a9ad65b30d05c42784bfad8e4095fe8d027
Mimo <mimo@example.com>
2026-04-14 16:32:15 +0200

    Mise a jour ZONE 14: Scanner et Liste

--- lib/scanner_screen.dart
+++ lib/scanner_screen.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/services.dart';
